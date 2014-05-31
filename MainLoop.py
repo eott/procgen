d1c6b5f44b332094c971cdddc33bf8bf1a1091b9
Etienne Ott <eti.ott@gmail.com>
2014-05-31 14:57:08 +0200

    Changed main loop to use class GameState instead of simple flags.

--- MainLoop.py
+++ MainLoop.py
@@ -8,21 +8,23 @@ pygame.init()
 colorBlack = pygame.Color(0, 0, 0)
 colorWhite = pygame.Color(255, 255, 255)
 
+gameState = GameState()
+
 screen = pygame.display.set_mode((1024, 758))
 pygame.display.set_caption('Procedural Generation Viewer')
 clock = pygame.time.Clock()
 
 font = pygame.font.Font('freesansbold.ttf', 32)
 
-keepRunning = True
-while keepRunning:
+gameState.advanceCurrentState()
+while gameState.getCurrentState() == gameState.GAME_STATE_RUNNING:
     levelName = font.render('Testlevel', False, colorWhite)
     rectangle = levelName.get_rect()
     screen.blit(levelName, rectangle)
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            keepRunning = False
+            gameState.advanceCurrentState()
 
     pygame.display.update()
     deltaTime = clock.tick(FRAMES_PER_SECOND)
