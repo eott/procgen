953eba5bc4db5556df3261db79831719b2f0d686
Etienne Ott <eti.ott@gmail.com>
2014-06-01 23:39:05 +0200

    Added camera class to encapsulate information about the camera position and the field of view.

---
+++ Camera.py
@@ -0,0 +1,30 @@
+class Camera:
+
+    ZOOM_STEP = 0.25
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.zoom = 1.0
+        self._width = 15 # tiles, not pixel
+        self._height = 9 # tiles, not pixel
+
+    def moveTo(self, x, y):
+        self.x = x
+        self.y = y
+
+    def moveBy(self, x, y):
+        self.x += x
+        self.y += y
+
+    def incZoom(self):
+        self.zoom += Camera.ZOOM_STEP
+
+    def decZoom(self):
+        self.zoom -= Camera.ZOOM_STEP
+
+    def getWidth(self):
+        return (self._width * self.zoom).ceil()
+
+    def getHeight(self):
+        return (self._height * self.zoom).ceil()
