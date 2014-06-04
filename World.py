b4f9c3e1939ade0c51fb890c51c81c426fa00849
Etienne Ott <eti.ott@gmail.com>
2014-06-04 22:02:25 +0200

    Added support for world and object data in primitive file format during world loading.

--- World.py
+++ World.py
@@ -7,11 +7,28 @@ class World:
     def __init__(self):
         self.objectImageMapping = World.initImageMapping()
         self.objects = []
+        self.objectDef = []
+        self.attributes = []
 
     def loadWorldFromFile(self, filename):
         with open(filename, 'r') as file:
             for line in file:
-                self.objects.append(World.parseObjectFromString(line))
+                if line[0] == '#':
+                    self.attributes.append(World.parseAttributeFromString(line))
+                elif line[0] == '@':
+                    self.objectDef.append(World.parseObjectDefFromString(line))
+                else:
+                    self.objects.append(World.parseObjectFromString(line))
+
+    @staticmethod
+    def parseAttributeFromString(strLine):
+        values = strLine[1:].split(';')
+        return Attribute(values[0], values[1])
+
+    @staticmethod
+    def parseObjectDefFromString(strLine):
+        values = strLine[1:].split(';')
+        return Definition(values[1], values[0], values[2])
 
     @staticmethod
     def parseObjectFromString(strLine):
@@ -25,6 +42,20 @@ class World:
             mapping[filename.split('.', 1)[0]] = pygame.image.load('images/'+filename)
         return mapping
 
+
+class Attribute:
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class Definition:
+
+    def __init__(self, name, imageName, description):
+        self.name = name
+        self.imageName = imageName
+        self.description = description
 
 class Tile:
 
