8c68c330ebd81f954a5fd9e30f4afb1db59172a4
Etienne Ott <eti.ott@gmail.com>
2014-06-22 18:58:44 +0200

    Extended Babel world generation to use multiple instance of basic room unit and translate the units.

--- Babel.py
+++ Babel.py
@@ -8,8 +8,17 @@ class LibraryOfBabel (WorldGeneration):
         WorldGeneration.__init__(self, seed)
 
     def _generateWorld(self):
-        unit = Unit("lob-room")
-        return unit
+        world = World()
+        for x in range(0, 21, 4):
+            for y in range(0, 21, 4):
+                unit = Unit("lob-room")
+                for objDef in unit.objectDef:
+                    if not world.objectDef.__contains__(objDef):
+                        world.objectDef.append(objDef)
+                for obj in unit.objects:
+                    translatedObj = Tile(obj.name, obj.x + x, obj.y + y, obj.z)
+                    world.objects.append(translatedObj)
+        return world
 
 
 class Unit(World):
